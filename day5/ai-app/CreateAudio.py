f855995d546c6e5b27797e2e5da9d35437f8e444
Stachlewski <stachlew@147dda7b25bb.ant.amazon.com>
2021-05-26 10:06:37 +0200

    ok

--- day5/ai-app/CreateAudio.py
+++ day5/ai-app/CreateAudio.py
@@ -89,8 +89,4 @@ def lambda_handler(event, context):
         }
     )
 
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return "OK"
